910557067a48d97878110a65e0b900bedbe9215a
darcrand <darcrandex@gmail.com>
2025-03-27 00:21:53 +0800

    🦄 refactor(app): clear

--- src/services/topic.ts
+++
@@ -1,7 +0,0 @@
-import { http } from '@/utils/http'
-
-export const topicService = {
-  pages: () => http.get<Array<API.PostItem>>('/posts'),
-
-  detail: (id: string) => http.get<API.PostItem>(`/posts/${id}`)
-}
--- src/stores/useUserInfo.ts
+++
@@ -1,9 +0,0 @@
-import { taroStorage } from '@/utils/atom-taro-storage'
-import { useAtom } from 'jotai'
-import { atomWithStorage } from 'jotai/utils'
-
-const stateAtom = atomWithStorage<API.UserInfo | null>('userInfo', null, taroStorage)
-
-export function useUserInfo() {
-  return useAtom(stateAtom)
-}

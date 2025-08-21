d15d82458419f0969ef1252270a853b68d251479
darcrand <darcrandex@gmail.com>
2025-08-22 00:34:08 +0800

    🎉 init(app): clear

--- src/stores/useMsg.ts
+++
@@ -1,9 +0,0 @@
-import { taroStorage } from '@/utils/atom-with-taro-storage'
-import { useAtom } from 'jotai/react'
-import { atomWithStorage } from 'jotai/utils'
-
-const msgAtom = atomWithStorage<string>('msg', 'hello', taroStorage)
-
-export function useMsg() {
-  return useAtom(msgAtom)
-}
--- src/utils/common.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 异步等待
- * @param ms 等待时间
- */
-export async function sleep(ms: number = 1000) {
-  return new Promise<void>((resolve) => setTimeout(resolve, ms))
-}
